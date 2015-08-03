f6bc5fa13e86138d3fcc92dfc2194d18c7ea3ff0
dhishi <dhishi17@hotmail.com>
2015-08-02 22:32:31 -0300

    teste note casa

--- WebApplication3/default.aspx.designer.cs
+++ WebApplication3/default.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace WebApplication3 {
         protected global::System.Web.UI.WebControls.Button BtnLista;
         
         /// <summary>
-        /// RadioButtonList1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
-        
-        /// <summary>
         /// DdlTipoRestaurante control.
         /// </summary>
         /// <remarks>
